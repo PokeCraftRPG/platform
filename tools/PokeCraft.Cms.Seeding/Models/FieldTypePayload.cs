b2dd2d16e36586c0c3ea0d64d635de29b6ab557f
Francis Pion <francispion@hotmail.com>
2026-04-23 11:56:50 -0400

    Implemented Seeding. (#2)

---
+++ tools/PokeCraft.Cms.Seeding/Models/FieldTypePayload.cs
@@ -0,0 +1,8 @@
+﻿using Krakenar.Contracts.Fields;
+
+namespace PokeCraft.Cms.Seeding.Models;
+
+internal record FieldTypePayload : CreateOrReplaceFieldTypePayload
+{
+  public Guid Id { get; set; }
+}
---
+++ tools/PokeCraft.Cms.Seeding/Tasks/InitializeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Krakenar.Core.Configurations.Commands;
+using Logitar.CQRS;
+using PokeCraft.Cms.Seeding.Settings;
+
+namespace PokeCraft.Cms.Seeding.Tasks;
+
+internal class InitializeConfigurationTask : SeedingTask
+{
+  public override string? Description => "Migrates Krakenar database.";
+}
+
+internal class InitializeConfigurationTaskHandler : ICommandHandler<InitializeConfigurationTask, Unit>
+{
+  private readonly DefaultSettings _defaults;
+  private readonly ICommandHandler<InitializeConfiguration, Unit> _handler;
+
+  public InitializeConfigurationTaskHandler(DefaultSettings defaults, ICommandHandler<InitializeConfiguration, Unit> handler)
+  {
+    _defaults = defaults;
+    _handler = handler;
+  }
+
+  public async Task<Unit> HandleAsync(InitializeConfigurationTask task, CancellationToken cancellationToken)
+  {
+    InitializeConfiguration command = new(_defaults.Locale, _defaults.UniqueName, _defaults.Password);
+    await _handler.HandleAsync(command, cancellationToken);
+
+    return Unit.Value;
+  }
+}
