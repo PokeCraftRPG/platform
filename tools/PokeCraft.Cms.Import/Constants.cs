6454ba8c6b83480f822397e9439ab281af2a73c5
Francis Pion <francispion@hotmail.com>
2026-04-28 18:40:31 -0400

    Deleted the importer.

--- tools/PokeCraft.Cms.Import/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PokeCraft.Cms.Import;
-
-internal static class Constants
-{
-  public const string Language = "en";
-  public const string NationalPokedex = "national";
-  public const string VersionGroup = "scarlet-violet";
-}
